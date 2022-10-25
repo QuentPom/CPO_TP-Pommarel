9e0ea42f0bc989b91915eb138564bebf5c5b644c
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-10-25 16:47:12 +0200

    premiere partie TP2

---
+++ com.mycompany_TP2_Biere_Pommarel_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/tp2_biere_pommarel/TP2_Biere_Pommarel.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.tp2_biere_pommarel;
+
+/**
+ *
+ * @author quent
+ */
+public class TP2_Biere_Pommarel {
+
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
+                7.0, "Dubuisson");
+
+        uneBiere.lireEtiquette();
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,
+                 "Abbaye de Leffe");
+        autreBiere.lireEtiquette();
+        BouteilleBiere autre2Biere = new BouteilleBiere("Pelfort", 6.5, "Heineken France");
+        autre2Biere.lireEtiquette();
+        BouteilleBiere autre3Biere = new BouteilleBiere("Kronembourg", 4.2, "Kronembourg SAS");
+        autre3Biere.lireEtiquette();
+        BouteilleBiere autre4Biere = new BouteilleBiere("Guiness", 4.2, "Guiness");
+        autre4Biere.lireEtiquette();
+        uneBiere.decapsuler();
+        autreBiere.decapsuler();
+        autre2Biere.decapsuler();
+        autre3Biere.decapsuler();
+        
+        System.out.println(uneBiere) ;
+        System.out.println(autreBiere) ;
+        System.out.println(autre2Biere) ;
+        System.out.println(autre3Biere) ;
+        System.out.println(autre4Biere) ;
+        
+    }
+}
