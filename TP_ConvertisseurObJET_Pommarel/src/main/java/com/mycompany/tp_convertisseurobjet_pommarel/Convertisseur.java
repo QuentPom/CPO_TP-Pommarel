6c44fd9010b11c89d1495dd49d95518f879da8f8
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-10-28 16:13:37 +0200

    Fin de l'exercice 2 avec la possibilité de choisir la conversion et de choisir la valeur a convertir

---
+++ TP_ConvertisseurObJET_Pommarel/src/main/java/com/mycompany/tp_convertisseurobjet_pommarel/Convertisseur.java
@@ -0,0 +1,83 @@
+package com.mycompany.tp_convertisseurobjet_pommarel;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author quent
+ */
+public class Convertisseur {
+    double nbConversions = 0;
+    @Override
+    public String toString () {
+ return "nb de conversions"+ nbConversions;
+    }
+
+
+
+    public double CelciusVersKelvin(double celcius){
+
+        double Kelvin = celcius + 273.15;
+        nbConversions += 1;
+        return Kelvin;
+
+    }
+
+
+    public double KelvinVersCelcius(double kelvin){
+
+        double celcius = kelvin - 273.15;
+        nbConversions += 1;
+
+        return celcius;
+
+    }
+
+
+    public double FarenheitVersCelcius(double farenheit){
+
+        double celcius = (farenheit - 32) / 1.8;
+        nbConversions += 1;
+
+        return celcius;
+
+    }
+
+
+    public double CelciusVersFarenheit(double celcius){
+
+        double farenheit = (celcius * 1.8) + 32;
+        nbConversions += 1;
+
+        return farenheit;
+
+    }
+
+
+    public double KelvinVersFarenheit(double kelvin){
+
+        double farenheit = CelciusVersFarenheit(KelvinVersCelcius(kelvin));
+        nbConversions += 1;
+
+        return farenheit;
+
+    }
+
+
+    public double FarenheitVersKelvin(double farenheit){
+
+        double kelvin = CelciusVersKelvin(FarenheitVersCelcius(farenheit));
+        nbConversions += 1;
+
+        return kelvin;
+
+    }
+
+}
+    
+
