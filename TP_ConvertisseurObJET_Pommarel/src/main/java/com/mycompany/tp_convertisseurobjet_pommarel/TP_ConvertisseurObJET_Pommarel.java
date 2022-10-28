2f9ae66d5d76847063d388bd19b31d9f50fa51f1
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-10-28 15:28:14 +0200

    Essaie de plusieurs conversions pour voir si le nbconversions marche

--- TP_ConvertisseurObJET_Pommarel/src/main/java/com/mycompany/tp_convertisseurobjet_pommarel/TP_ConvertisseurObJET_Pommarel.java
+++ TP_ConvertisseurObJET_Pommarel/src/main/java/com/mycompany/tp_convertisseurobjet_pommarel/TP_ConvertisseurObJET_Pommarel.java
@@ -13,9 +13,13 @@ public class TP_ConvertisseurObJET_Pommarel {
 
     public static void main(String[] args) {
         double res;
+        double res1;
         Convertisseur Monconvertisseur = new Convertisseur();
         res = Monconvertisseur.CelciusVersKelvin(18.0);
-        System.out.println(res) ;
+        res1 = Monconvertisseur.FarenheitVersCelcius(20);
+        System.out.println(res);
+        System.out.println(res1);
+        System.out.println(Monconvertisseur) ;
 
     }
 }
