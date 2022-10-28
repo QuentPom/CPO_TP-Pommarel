a007655f473e29656c32de72f6aa52ac94eb098c
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-10-28 16:28:00 +0200

    debut de l'exo 3

---
+++ T_manip_Pommarel/src/main/java/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author quent
+ */
+public class Tartiflette {
+    int nbCalories;
+
+    public Tartiflette(int nbCalories){
+        nbCal = nbCalories;
+    }
+}
