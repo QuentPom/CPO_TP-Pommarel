46b128c831e37b52233a7b1c0a4bc67f67d9db05
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-10-28 15:17:57 +0200

    Ajout de l'objet dans le main

---
+++ TP_ConvertisseurObJET_Pommarel/src/main/java/com/mycompany/tp_convertisseurobjet_pommarel/TP_ConvertisseurObJET_Pommarel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp_convertisseurobjet_pommarel;
+
+/**
+ *
+ * @author quent
+ */
+public class TP_ConvertisseurObJET_Pommarel {
+
+    public static void main(String[] args) {
+        double res;
+        Convertisseur Monconvertisseur = new Convertisseur();
+        res = Monconvertisseur.CelciusVersKelvin(18.0);
+        System.out.println(res) ;
+
+    }
+}
