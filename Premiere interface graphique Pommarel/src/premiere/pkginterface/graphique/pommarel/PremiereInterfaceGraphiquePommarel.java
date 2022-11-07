323532ec6c7e25c24d7884869a8888477fd508b6
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-11-07 19:56:31 +0100

    Interface graphique quizz

--- Premiere interface graphique Pommarel/src/premiere/pkginterface/graphique/pommarel/PremiereInterfaceGraphiquePommarel.java
+++ Premiere interface graphique Pommarel/src/premiere/pkginterface/graphique/pommarel/PremiereInterfaceGraphiquePommarel.java
@@ -10,11 +10,6 @@ package premiere.pkginterface.graphique.pommarel;
  */
 public class PremiereInterfaceGraphiquePommarel {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
+
 }
